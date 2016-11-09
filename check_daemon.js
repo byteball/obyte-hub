86bfb3ea6ecadbdca8f1d939fa07c00c1e80e502
tony <tonyofbyteball@users.noreply.github.com>
2016-11-10 01:31:39 +0300

    redirect stderr

--- check_daemon.js
+++ check_daemon.js
@@ -2,5 +2,5 @@
 "use strict";
 var check_daemon = require('byteballcore/check_daemon.js');
 
-check_daemon.checkDaemonAndRestart('node start.js', 'node start.js > log');
+check_daemon.checkDaemonAndRestart('node start.js', 'node start.js 1>log 2>>errlog');
 
