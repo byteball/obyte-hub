ffe5cd6dc9c0e7f413fe6b64709953fdaf7a6af2
tony <tonyofbyteball@users.noreply.github.com>
2017-06-02 01:11:16 +0300

    initial bots

---
+++ bots.sql
@@ -0,0 +1,30 @@
+INSERT INTO bots (name, description, pairing_code) VALUES (
+	'Transition bot', 
+	'Allows you to link your Bitcoin and Byteball addresses for participation in Byteball distribution.', 
+	'A2WMb6JEIrMhxVk+I0gIIW1vmM3ToKoLkNF8TqUV5UvX@byteball.org/bb#0000'
+);
+INSERT INTO bots (name, description, pairing_code) VALUES (
+	'Byte-BTC exchange', 
+	'Buy or sell Bytes for BTC instantly. Advanced users can offer their price, post pending orders and see the order book.', 
+	'Ar2ukVqx309sX+LoC9RVOpfATgXskt+Ser5jVr3Q2FOo@byteball.org/bb#0000'
+);
+INSERT INTO bots (name, description, pairing_code) VALUES (
+	'Flight delays oracle', 
+	'If you bought a P2P insurance against flight delays and your flight was delayed, chat with this oracle to have it post the data about your flight. After the data is posted, you can unlock the insurance contract and sweep its funds.', 
+	'AuP4ngdv0S/rok+IaW1q2D6ye72eXLl3h+CqXNXzkBXn@byteball.org/bb#0000'
+);
+INSERT INTO bots (name, description, pairing_code) VALUES (
+	'Sports oracle', 
+	'If you have a P2P betting contract with another user and you won, chat with this oracle to have it post the data about your football match. After the data is posted, you can unlock the betting contract and sweep its funds.', 
+	'Ar1O7dGgkkcABYNAbShlY2Pbx6LmUzoyRh6F14vM0vTZ@byteball.org/bb#0000'
+);
+INSERT INTO bots (name, description, pairing_code) VALUES (
+	'BTC oracle', 
+	'This oracle posts Merkle Roots of all Bitcoin transactions in a block every time a new Bitcoin block is mined. You can use its data to P2P trade Bytes vs BTC. If you are receiving Bytes (sending bitcoins), chat with the oracle after sending your bitcoins to get the Merkle Proof of your Bitcoin transacton and unlock your bytes from the smart contract.', 
+	'A7C96Bhg4Gpb2Upw/Ky/YfGG8BKe5DjTiBuJFGAX50N1@byteball.org/bb#0000'
+);
+INSERT INTO bots (name, description, pairing_code) VALUES (
+	'Bounce bot', 
+	'Just for fun, send some of your bytes to the bot, and they will be bounced back to you. Developer: vakar.', 
+	'A6WUhxQX7bT1xY5UxoeL2/zNEgXGQXRO5ze4msXu3QrF@byteball.com.ar/bb#0000'
+);
