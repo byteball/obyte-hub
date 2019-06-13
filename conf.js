c1201b432a0b85b8418acc2353fca5cc7c008a51
tony <tonyofbyteball@users.noreply.github.com>
2019-06-13 10:23:42 +0300

    2.7.2

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@
 "use strict";
 
 exports.clientName = 'byteball';
-exports.minClientVersion = '2.6.0';
+exports.minClientVersion = '2.7.2';
 
 // https://console.developers.google.com
 exports.pushApiProjectNumber = 0;
