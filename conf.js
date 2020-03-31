741f9cf96b490bf3b811cf949c46eb3fe8e46088
tony <tonyofbyteball@users.noreply.github.com>
2020-03-31 22:19:14 +0300

    fixed token registry

--- conf.js
+++ conf.js
@@ -64,7 +64,7 @@ exports.initial_peers = [
 
 exports.trustedRegistries = {
 	'AM6GTUKENBYA54FYDAKX2VLENFZIMXWG': { name: 'market' },
-	'VGR3HJBD5LKSVIBAM7BRC22DBCURPZCI': { name: 'DTR', allow_updates: true },
+	'Y6RR2PZJFLL7BNBPQJSXY656NMF2Y3A5': { name: 'DTR', allow_updates: true },
 };
 
 exports.known_witnesses = process.env.testnet ? null : {
