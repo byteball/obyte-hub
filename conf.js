f5cb4f4ce8d93095cff0fcea75b6b9cca3bed3c5
tony <tonyofbyteball@users.noreply.github.com>
2020-04-21 22:35:53 +0300

    no bug sink emails by default

--- conf.js
+++ conf.js
@@ -21,8 +21,8 @@ exports.bSaveJointJson = true;
 exports.bLight = false;
 
 // this is used by wallet vendor only, to redirect bug reports to developers' email
-exports.bug_sink_email = 'admin@example.org';
-exports.bugs_from_email = 'bugs@example.org';
+exports.bug_sink_email = ''; // 'admin@example.org';
+exports.bugs_from_email = ''; // 'bugs@example.org';
 
 exports.HEARTBEAT_TIMEOUT = 300*1000;
 
