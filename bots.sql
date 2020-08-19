a349bbcd9f7e59c0a3e5fea96537de21d707733b
tony <tonyofbyteball@users.noreply.github.com>
2020-08-19 14:45:36 +0300

    github attestation bot

--- bots.sql
+++ bots.sql
@@ -270,3 +270,9 @@ INSERT INTO bots (name, description, pairing_code) VALUES (
 If you want to exchange more crypto assets, please visit OSwap exchange: https://oswap.cc.',
    'A2hyUbe/RePfAKHC5eY8/lAI17jgkVysxx9wpE+9dcwz@hub.obytechina.org/bb#0000'
 );
+INSERT INTO bots (rank, name, description, pairing_code) VALUES (
+	2,
+   'GitHub attestation bot',
+   'Verify your GitHub username and link it to your Obyte address. Also works for organizations. In a future release, you will be able to receive payments to github/username alias instead of the regular Obyte address.',
+   'A7mopY9L4F+/G+CUMjxFm8royoFl2hde5n9oUWVUkfEI@obyte.org/bb#0000'
+);
