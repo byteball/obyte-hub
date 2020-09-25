af9a95a7d9ffe889996b3fc760b7926917677ad2
Tarmo <tarmo888@hotmail.com>
2020-09-25 03:55:22 +0300

    same sources as oracle

--- exchange_price_feed.js
+++ exchange_price_feed.js
@@ -6,32 +6,51 @@ const eventBus = require('ocore/event_bus.js');
 const network = require('ocore/network.js');
 require("tls").DEFAULT_ECDH_CURVE = "auto"; // fix for Node 8
 
-const symbols = ['USDT-BTC', 'BTC-GBYTE'];
 const rates = {};
 
-function updateBittrexRates(state, onDone) {
-	const apiUri = 'https://bittrex.com/api/v1.1/public/getmarketsummaries';
+function updateBitfinexRates(state, onDone) {
+	const apiUri = 'https://api.bitfinex.com/v1/pubticker/btcusd';
 	request(apiUri, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			let arrCoinInfos;
-			try {arrCoinInfos = JSON.parse(body).result;} catch(e){}
-			if (!arrCoinInfos) {
-				console.log('bad rates from bittrex');
+			let price;
+			try{
+				price = parseFloat(JSON.parse(body).last_price);
+				console.log("new exchange rate: BTC-USD = " + price);
+			}
+			catch(e){
+				console.log('bad response from bitfinex:', e);
 				return onDone();
 			}
-			let prices = {};
-			arrCoinInfos.forEach(coinInfo => {
-				if (!coinInfo.Last)
-					return;
-				if (symbols.includes(coinInfo.MarketName)) {
-					prices[coinInfo.MarketName] = coinInfo.Last;
-					console.log("new exchange rate: " + coinInfo.MarketName + " = " + coinInfo.Last);
+			if (price) {
+				rates['BTC_USD'] = price;
+				state.updated = true;
+			}
+		}
+		else {
+			console.error("Can't get currency rates from bitfinex", error, body);
+		}
+		onDone();
+	});
+}
+
+function updateBittrexRates(state, onDone) {
+	const apiUri = 'https://api.bittrex.com/v3/markets/GBYTE-BTC/ticker';
+	request(apiUri, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			let price;
+			try{
+				price = parseFloat(JSON.parse(body).lastTradeRate);
+				console.log("new exchange rate: GBYTE-BTC = " + price);
+			}
+			catch(e){
+				console.log('bad response from bittrex:', e);
+				return onDone();
+			}
+			if (price) {
+				rates['GBYTE_BTC'] = price;
+				if (rates['BTC_USD']) {
+					rates['GBYTE_USD'] = price * rates['BTC_USD'];
 				}
-			});
-			if (Object.keys(prices).length == symbols.length) {
-				rates['BTC_USD'] = prices['USDT-BTC'];
-				rates['GBYTE_BTC'] = prices['BTC-GBYTE'];
-				rates['GBYTE_USD'] = prices['BTC-GBYTE'] * prices['USDT-BTC'];
 				state.updated = true;
 			}
 		}
@@ -154,6 +173,9 @@ function updateBTC_20200701Rates(state, onDone) {
 function updateRates(){
 	let state = {updated: false};
 	async.series([
+		function(cb){
+			updateBitfinexRates(state, cb);
+		},
 		function(cb){
 			updateBittrexRates(state, cb);
 		},
