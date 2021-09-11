7f1da6ecce820453ce15b4e38ea7c2c8ff36b865
tony <tonyofbyteball@users.noreply.github.com>
2021-09-12 02:35:50 +0300

    3.4.0

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@
 "use strict";
 
 exports.clientName = 'byteball';
-exports.minClientVersion = '3.3.1';
+exports.minClientVersion = '3.4.0';
 exports.minClientVersionForChat = '3.0.3';
 
 // https://console.developers.google.com
