f0987b57ad83766e48692cea62dbac9965552432
tony <tonyofbyteball@users.noreply.github.com>
2022-05-04 14:57:39 +0300

    arbiter watcher simplified

--- arbregistry.js
+++ arbregistry.js
@@ -1,31 +1,38 @@
 const db = require('ocore/db');
 const conf = require('ocore/conf');
 const eventBus = require('ocore/event_bus.js');
+const validationUtils = require('ocore/validation_utils.js');
 
-if (!conf.arbstores)
-	return;
-let arbstore_addresses = Object.keys(conf.arbstores);
-if (arbstore_addresses.length === 0)
-	return;
-
-// snipe for arbiter announces on arbstores
-eventBus.on('mci_became_stable', async mci => {
-	let rows = await db.query(
-		`SELECT unit, payload, address
-		FROM units
-		JOIN messages USING(unit)
-		JOIN unit_authors USING(unit)
-		WHERE main_chain_index=? AND payload LIKE '{"address":%' AND address IN (?)`, [mci, arbstore_addresses]);
-	rows.forEach(async row => {
-		//let arbstore_url = conf.arbstores[row.address];
-		let arbiter_address = row.payload.match(/"address":"([^"]+)"/);
-		if (!arbiter_address)
-			return;
-		db.query("SELECT arbiter_address FROM arbiter_locations WHERE arbiter_address=?", [arbiter_address[1]], rows => {
-			if (!rows.length)
-				db.query("INSERT INTO arbiter_locations (arbiter_address, arbstore_address, unit) VALUES (?, ?, ?)", [arbiter_address[1], row.address, row.unit]);
+const arbstore_addresses = Object.keys(conf.arbstores || {});
+if (arbstore_addresses.length > 0) {
+	// snipe for arbiter announces on arbstores
+	async function checkForArbiterAnnouncements(mci) {
+		const rows = await db.query(
+			`SELECT unit, payload, address
+			FROM units
+			CROSS JOIN messages USING(unit)
+			CROSS JOIN unit_authors USING(unit)
+			WHERE main_chain_index=? AND app='data' AND payload LIKE '{"address":%' AND address IN (?)`,
+			[mci, arbstore_addresses]
+		);
+		for (let { unit, payload, address: arbstore_address } of rows) {
+			//let arbstore_url = conf.arbstores[arbstore_address];
+			const matches = payload.match(/"address":"([^"]+)"/);
+			if (!matches) {
+				console.log('arbiter address not found in payload', payload, 'of unit', unit, 'from', arbstore_address);
+				continue;
+			}
+			const arbiter_address = matches[1];
+			if (!validationUtils.isValidAddress(arbiter_address)) {
+				console.log('invalid arbiter address', arbiter_address, 'in', unit, 'from', arbstore_address);
+				continue;
+			}
+			const [existing_record] = await db.query("SELECT arbiter_address FROM arbiter_locations WHERE arbiter_address=?", [arbiter_address]);
+			if (!existing_record)
+				await db.query("INSERT INTO arbiter_locations (arbiter_address, arbstore_address, unit) VALUES (?, ?, ?)", [arbiter_address, arbstore_address, unit]);
 			else
-				db.query("UPDATE arbiter_locations SET arbstore_address = ?, unit = ? WHERE arbiter_address = ?", [row.address, row.unit, arbiter_address[1]]);
-		});
-	});
-});
+				await db.query("UPDATE arbiter_locations SET arbstore_address = ?, unit = ? WHERE arbiter_address = ?", [arbstore_address, unit, arbiter_address]);
+		}
+	}
+	eventBus.on('mci_became_stable', checkForArbiterAnnouncements);
+}
